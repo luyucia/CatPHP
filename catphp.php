5df4e546ef347afce06c48b58b9e786897bb04d9
unknown <limingze@limingze.dangdang.com>
2014-05-29 10:51:54 +0800

    测试git

--- catphp.php
+++ catphp.php
@@ -2,6 +2,7 @@
 
 define('CAT_START_TIME'  , microtime(true)    );
 define('CAT_START_MEMORY', memory_get_usage() );
+
 define('CAT_BASE'        , dirname(__FILE__)  );
 require CAT_BASE.'/core/core.php';
 
