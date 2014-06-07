a6cc9524f2066a64ff6a660ed33793a85d01eff3
luyucia <luyucia@gmail.com>
2014-06-07 10:52:10 +0800

    add comment

--- catphp/catphp.php
+++ catphp/catphp.php
@@ -1,8 +1,8 @@
 <?php
-
+// 此处定义了两个常量，保存初始时刻的时间和内存使用
 define('CAT_START_TIME'  , microtime(true)    );
 define('CAT_START_MEMORY', memory_get_usage() );
-
+// CAT_BASE为框架根目录，定义这个的目的是方便后面使用绝对地址载入php文件。
 define('CAT_BASE'        , dirname(__FILE__)  );
 require CAT_BASE.'/core/core.php';
 
