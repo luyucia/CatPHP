3c003ceed685b71cf8721822d58d2e08651437dd
JasonLeemz <limingze610@gmail.com>
2014-06-10 16:25:02 +0800

    上次提交漏掉了文件

---
+++ catphp/bricks/mvc/web_common_class.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * 工具类
+ * author:JasonLee
+ * email:limingze610@gmail.com
+ */
+class Tools {
+
+    public $filter_arr = array();
+
+    public function __construct($array = array()) {
+        $this->filter_arr = $array;
+    }
+
+    /**
+     * 将传入的数组每个元素两端的空格或指定的字符串过滤掉
+     * @return \Tools
+     */
+    public function F($character_mask = null) {
+        if(empty($character_mask)){
+            foreach ($this->filter_arr as $key => $value) {
+                $this->filter_arr[$key] = trim($value);
+            }
+        }else{
+            foreach ($this->filter_arr as $key => $value) {
+                $this->filter_arr[$key] = trim($value,$character_mask);
+            }
+        }
+        
+        return $this;
+    }
+
+    /**
+     * 使用addslashes函数对指定的array进行转义
+     * @return \Tools
+     */
+    public function R() {
+        foreach ($this->filter_arr as $key => $value) {
+            $this->filter_arr[$key] = addslashes($value);
+        }
+        return $this;
+    }
+
+}
