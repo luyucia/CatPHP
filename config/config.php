d09e876016b3e4e02c0865fcd1985120c5625e75
王旭 <play89925@gmail.com>
2014-06-25 20:39:32 +0800

    Update config.php
    尽量使用兼容的写法吧

--- config/config.php
+++ config/config.php
@@ -1,14 +1,16 @@
 <?php
 
-return [
-    'view_path' => 'views/',
-    'controller_path' => 'controllers/',
-    'controller_dirs' => ['test/','api/'],
-    'router_rest' => true
-    'router_name' => [
+return array(
+    'view_path'=>'views/',
+    'controller_path'=>'controllers/',
+    'controller_dirs' => array('test/','api/'),
+    'router_rest'     => true, 
+    'router_name'     =>array(
             'c' => 'c',
             'a' => 'a',
-        ], 
+        ), 
     // 模板引擎 可选（tenjin smarty php）
-    'template_engine' => 'tenjin'
-    ];
+    'template_engine'=>'tenjin'
+    );
+
+?>
