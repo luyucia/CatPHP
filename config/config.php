64c5eccbe05820773a0d8d1442ec976381d31451
baodude <79403828@qq.com>
2014-06-25 16:25:09 +0800

    格式化代码，使用短语法[...元素...]方式声明数组

--- config/config.php
+++ config/config.php
@@ -1,18 +1,14 @@
 <?php
 
-return array(
-    'view_path'=>'views/',
-    'controller_path'=>'controllers/',
-    'controller_dirs' => array('test/','api/'),
-    'router_rest'     => true, 
-    'router_name'     =>array(
+return [
+    'view_path' => 'views/',
+    'controller_path' => 'controllers/',
+    'controller_dirs' => ['test/','api/'],
+    'router_rest' => true
+    'router_name' => [
             'c' => 'c',
             'a' => 'a',
-        ), 
+        ], 
     // 模板引擎 可选（tenjin smarty php）
-    'template_engine'=>'tenjin'
-    );
-
-
-
-?>
+    'template_engine' => 'tenjin'
+    ];
