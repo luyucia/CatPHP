0dee675e43ef81fa8f8e82f7846f64ce42766489
luyucia <luyucia@gmail.com>
2015-05-03 16:35:48 +0800

    完善文档页

--- example/controllers/index.php
+++ example/controllers/index.php
@@ -11,8 +11,29 @@ class indexController extends Controller {
      */
     public function index($name = "Stranger") {
         echo "<h1>Welcome to Use CatPHP</h1>";
-        $this->assign("title",'Welcome');
-        $this->render('views/index.tpl');
+        $text = <<<EOF
+
+### 第一步：下载CatPHP
+下载地址：https://github.com/luyucia/CatPHP
+### 第二步：将样例工程复制到您的web服务器工作目录下
+将example目录复制到您的工作目录下（nginx默认为html）
+### 第三步：配置Nginx或Apache的路由重写规则
+假如您使用Nginx则：
+请在nginx目录下找到conf/nginx.conf文件，并添加重写规则：
+
+    if (!-e $request_filename) {
+            rewrite ^.*$ /index.php last;
+        }
+    
+这样nginx会将所有请求不到的url请求发送到 index。php
+
+完成：现在您就可以在浏览器中访问您的项目了 exp：http://127.0.0.1
+
+EOF;
+        // $this->assign("title",'Welcome');
+        // $this->render('views/index.tpl');
+		$mk = new Parsedown();
+		echo $mk->parse($text);
 
 
     }
---
+++ example/templates_c/0d4c522f969d7cfda392090351533e3f100a94e9.file.index.tpl.php
@@ -0,0 +1,35 @@
+<?php /* Smarty version Smarty-3.1.21-dev, created on 2015-05-01 17:00:56
+         compiled from "views\index.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:2323554340c8ae7d10-09174583%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '0d4c522f969d7cfda392090351533e3f100a94e9' => 
+    array (
+      0 => 'views\\index.tpl',
+      1 => 1430468873,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '2323554340c8ae7d10-09174583',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'title' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.21-dev',
+  'unifunc' => 'content_554340c8b099c4_81937643',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_554340c8b099c4_81937643')) {function content_554340c8b099c4_81937643($_smarty_tpl) {?><html>
+<meta charset=utf-8>
+<meta name=description content="">
+<meta name=viewport content="width=device-width, initial-scale=1">
+  <body>
+    <h1><?php echo $_smarty_tpl->tpl_vars['title']->value;?>
+</h1>
+    
+  </body>
+</html><?php }} ?>
