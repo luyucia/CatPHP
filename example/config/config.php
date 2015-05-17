55b4e7eef03aa637a38a780b53335ea8ce7fa519
卢禹 <luyu@luyudeMacBook-Pro.local>
2015-05-18 00:12:28 +0800

    document

--- example/config/config.php
+++ example/config/config.php
@@ -1,16 +1,22 @@
 <?php
 
 return array(
+    // 模板文件路径
     'view_path'=>'views/',
+    // 控制器文件路径，controller都会在这个目录下查找
     'controller_path'=>'controllers/',
+    // 控制器子目录，如果希望在控制器目录下放子目录可在此设置
     'controller_dirs' => array('test/','api/'),
+    // 设置哪些controller为RESTfule形式
     'rest_controllers'=>array('product','api'),
+    // 设置路由模式，router_rest为true时，采用Rest风格路由，为false则采用参数形式，router_name参数生效
     'router_rest'     => true,
-    'log_level'       => Logging::ALL,
     'router_name'     =>array(
             'c' => 'c',
             'a' => 'a',
         ),
+    // 设置日志等级
+    'log_level'       => Logging::ALL,
     // 模板引擎 可选（tenjin smarty php）
     'template_engine'=>array(
         'type'  => 'smarty',
@@ -18,6 +24,7 @@ return array(
         'cache' => false,
         'cache_lifetime' => 120,
     ),
+    // 设置路由规则，详细内容请参考文档中的路由专题
     'route_rules'=>array(
         // 静态路由 和参数
             array(
