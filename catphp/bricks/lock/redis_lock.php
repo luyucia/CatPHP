a70414e3f499f2317a3d4046e21a6ff8b67990dd
limingze <limingze@163.com>
2016-08-30 14:59:15 +0800

    edit redis lock

--- catphp/bricks/lock/redis_lock.php
+++ catphp/bricks/lock/redis_lock.php
@@ -17,11 +17,35 @@ class RedisLock
     {
         $this->lockKey = 'catlock:'.$key;
         // 如果能设置,说明获取锁成功
-        $unlock = $this->redis->setnx($this->lockKey,1);
-        if ($unlock) {
-            $this->redis->expire($this->lockKey ,$expire);
+        // $expireTime  = time()+$expire;
+        // $lock = $this->redis->setnx($this->lockKey,$expireTime);
+
+        // if ($lock) {
+        //     $this->redis->expire($this->lockKey ,$expire);
+        //     return true;
+        // }
+
+        while (1) {
+            $expireTime  = time()+$expire;
+            // 如果能设置,说明获取锁成功
+            $lock = $this->redis->setnx($this->lockKey,$expireTime);
+
+            if ($lock) {
+                // 得到锁
+                $this->redis->expire($this->lockKey ,$expire);
+                return true;
+            }else{
+                // 没得到
+                $lockExpireTime = $this->redis->get($this->lockKey);
+                if(time() > $lockExpireTime){
+                    // 如果过期了 主动解锁
+                    $this->unlock();
+                }else{
+                    // 没过期则继续等待
+                    usleep(10);
+                }
+            }
         }
-        return $unlock;
     }
 
     public function unlock()
